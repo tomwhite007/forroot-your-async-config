4259ba86588f7538d7ffdf0269d932a5ad5466a8
Tom White <tomwhite007@gmail.com>
2021-02-22 16:38:10 +0000

    fix tests v2

--- apps/injectors-demo/src/app/app.component.spec.ts
+++ apps/injectors-demo/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigService } from './services/config.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [
+        { provide: ConfigService, useValue: { getConfig: () => 'test123' } },
+      ],
       imports: [HttpClientTestingModule],
     }).compileComponents();
   });
